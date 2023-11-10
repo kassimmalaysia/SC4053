b53164778c3e6ba0d31a32ab747908f4932c63a0
kassimmalaysia <101180214+kassimmalaysia@users.noreply.github.com>
2023-11-10 22:55:21 +0800

    Got the HighestBidder and Highest Bid

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import { parseEther, formatEther } from '@ethersproject/units';
 import Auction from './contracts/Auction.json';
 import logoImage from './Capture.png';
-const AuctionContractAddress = '0x16D175B4277028D84B3983E9f22bA4fef71F3182';
+const AuctionContractAddress = '0xf47575dbe289ed5d6dd74daa7d122fa370bca231';
 // The AuctionContract Address needs to change based on your remix contract everytime you deploy
 const emptyAddress = "0x0000000000000000000000000000000000000000";
 const ethers = require("ethers")
@@ -13,7 +13,7 @@ function App() {
  // Use hooks to manage component state
  const [account, setAccount] = useState('');
  const [domain, setDomain] = useState('');
- const [myBid, setMyBid] = useState();
+ const [myBid, setMyBid] = useState('');
  const [isOwner, setIsOwner] = useState(false);
  const [bidderCount, setBidderCount] = useState();
  const [highestBid, setHighestBid] = useState();
@@ -21,6 +21,15 @@ function App() {
  const [ownedDomains, setOwnedDomains] = useState([]);
  const [currentPhase, setCurrentPhase] = useState('');
 const [commitPhaseEndTime, setCommitPhaseEndTime] = useState(0);
+const [bidAmountUnit, setBidAmountUnit] = useState('wei');
+
+const handleBidAmountChange = (event) => {
+  setMyBid(event.target.value);
+};
+
+const handleBidUnitChange = (event) => {
+  setBidAmountUnit(event.target.value);
+};
 
  
  // Sets up a new Ethereum provider and returns an interface for interacting with the smart contract
@@ -40,97 +49,155 @@ const [commitPhaseEndTime, setCommitPhaseEndTime] = useState(0);
  async function requestAccount() {
    const account = await window.ethereum.request({ method: 'eth_requestAccounts' });
    setAccount(account[0]);
-  
-  
  }
+ const convertBidToWei = () => {
+  const bidValue = parseFloat(myBid);
 
- async function fetchHighestBid() {
-   if (typeof window.ethereum !== 'undefined') {
-     const contract = await initializeProvider();
-     try {
-      const [highestBidder, highestBid] = await contract.fetchHighestBid();
-       
-     // Convert bidAmount from Wei to Ether and round value to 4 decimal places
-        setHighestBid(parseFloat(formatEther(highestBid.toString())).toPrecision(4));
-        setHighestBidder(highestBidder);
-         
-      
-     } catch (e) {
-       console.log('error fetching highest bid: ', e);
-     }
-   }
- }
+  switch (bidAmountUnit) {
+    case 'wei':
+      return bidValue;
+    case 'gwei':
+      return bidValue * 1e9; // 1 Gwei = 1e9 Wei
+    case 'ether':
+      return bidValue * 1e18; // 1 Ether = 1e18 Wei
+    default:
+      return 0;
+  }
+};
 
+//  async function fetchMyBid() {
+//    if (typeof window.ethereum !== 'undefined') {
+//      const contract = await initializeProvider();
+//      try {
+//        const myBid = await contract.bids(account);
+//        setMyBid(parseFloat(formatEther(myBid.toString())).toPrecision(4));
+//      } catch (e) {
+//        console.log('error fetching my bid: ', e);
+//      }
+//    }
+//  }
 
- async function fetchMyBid() {
-   if (typeof window.ethereum !== 'undefined') {
-     const contract = await initializeProvider();
-     try {
-       const myBid = await contract.bids(account);
-       setMyBid(parseFloat(formatEther(myBid.toString())).toPrecision(4));
-     } catch (e) {
-       console.log('error fetching my bid: ', e);
-     }
-   }
- }
-
- async function fetchOwner() {
-   if (typeof window.ethereum !== 'undefined') {
-     const contract = await initializeProvider();
-     try {
-       const owner = await contract.getOwner();
-       setIsOwner(owner.toLowerCase() === account);
-     } catch (e) {
-       console.log('error fetching owner: ', e);
-     }
-   }
- }
+//  async function fetchOwner() {
+//    if (typeof window.ethereum !== 'undefined') {
+//      const contract = await initializeProvider();
+//      try {
+//        const owner = await contract.getOwner();
+//        setIsOwner(owner.toLowerCase() === account);
+//      } catch (e) {
+//        console.log('error fetching owner: ', e);
+//      }
+//    }
+//  }
 
  async function submitBid(event, domain, value, secret) {
   event.preventDefault();
   if (typeof window.ethereum !== 'undefined') {
     const contract = await initializeProvider();
     try {
-      // User inputs amount in terms of Ether, convert to Wei before sending to the contract.
-      const wei = parseEther(value.toString());
-      if (currentPhase === '' && Date.now() >= commitPhaseEndTime) {
+      console.log(value);
+      const ether =  value.toString();
+      
+      
         // await contract.startCommitPhase(domain);
-
         // Update the phase to Commit and set the commit phase end time
         setCurrentPhase('Commit');
-        setCommitPhaseEndTime(Date.now() + 60000); // 1 minute
-      }
-      // Replace the next line with the actual function call to commitBid
-      await contract.commitBid(domain, wei, "myhash");
-    
+        // Schedule revealBid to be called after the commit phase ends
 
+      console.log(ether);
+      // Replace the next line with the actual function call to commitBid
+      await contract.commitBid(domain,ether, "myhash");
     // Fetch the bidderCount when the user submits a bid
       fetchBidderCountForDomain(domain);
-     
-
       // Wait for the smart contract to emit the LogBid event, then update component state
-      contract.on('LogBid', (_, __) => {
-        fetchHighestBid();
+      contract.on('LogBid', (account, domain, ether, secret) => {
+        setTimeout(() => {
+          revealPhase(domain, ether, "myhash");
+        }, 60000); // 1 minute
+        
+        console.log(`New bid from ${account} for domain ${domain} with value ${ether} and secret ${secret}`);
       });
     } catch (e) {
       console.log('error making bid: ', e);
     }
   }
 }
- async function fetchBidderCountForDomain(domain) {
+ 
+async function revealPhase(domain, value,secret) {
+    if (typeof window.ethereum !== 'undefined') {
+      const contract = await initializeProvider();
+      try {
+         // Start RevealPhase
+        await contract.startRevealPhase();
+        console.log("in Start Reveal Phase",domain, value,secret);
+       
+        setCurrentPhase('Reveal');
+        // Update the phase to None
+        
+        setTimeout(() => {    
+         revealBid(domain, value,secret);  
+        }, 30000); // 0.5 minute
+
+      } catch (e) {
+        console.log('error starting revealing phase: ', e);
+      }
+    }
+} 
+async function revealBid(domain, value,secret) {
+    if (typeof window.ethereum !== 'undefined') {
+      const contract = await initializeProvider();
+      try {
+         // Start Reveal Bid
+         console.log("in Reveal Bid" ,domain, parseInt(value, 16),secret);
+        await contract.revealBid(domain, parseInt(value, 16),"myhash");
+        
+       
+      
+        setTimeout(() => {    
+        setCurrentPhase('Finalizing');
+        checkAndFinalizeAuction(domain); 
+          }, 30000); // 0.5 minute
+          
+        
+      } catch (e) {
+        console.log('error revealing bids: ', e);
+      }
+  }
+} 
+async function checkAndFinalizeAuction(domain) {
+    if (typeof window.ethereum !== 'undefined') {
+      const contract = await initializeProvider();
+      try {
+        console.log("in Finalizing Bid");
+        // Fetch the highest bid and highest bidder
+        await contract.finalizeAuction(domain);
+        fetchHighestBid(domain);
+        setTimeout(() => {    
+          // Update the phase to None
+          setCurrentPhase('Finalized');
+            }, 60000); // 1 minute
+      } catch (e) {
+        console.log('error finalizing auction: ', e);
+      }
+  }
+}
+
+async function fetchHighestBid(domain) {
   if (typeof window.ethereum !== 'undefined') {
     const contract = await initializeProvider();
     try {
-      // Simulate a delay (e.g., 1 seconds) before fetching the bidder count
-      setTimeout(async () => {
-        console.log(domain);
-        const updatedBidderCount = await contract.getBiddersCountByDomain(domain);
-        console.log('bidderCount:', updatedBidderCount.toNumber());
-        setBidderCount(updatedBidderCount.toNumber());
-      }, 20000); // Adjust the delay time as needed
+     const [highestBidder, highestBid] = await contract.fetchHighestBid(domain);
+     console.log("in fetchHighestBid");
+      
+    // Convert bidAmount from Wei to Ether and round value to 4 decimal places
+    setTimeout(() => {
+      setHighestBid(parseFloat(highestBid.toString()));
+      setHighestBidder(highestBidder);
+    }, 20000); 
+       
+        
      
     } catch (e) {
-      console.log(`Error fetching bidder count for domain ${domain}:`, e);
+      console.log('error fetching highest bid: ', e);
     }
   }
 }
@@ -148,23 +215,24 @@ const [commitPhaseEndTime, setCommitPhaseEndTime] = useState(0);
       }
     }
   }
-  async function checkAndFinalizeAuction() {
-    if (currentPhase === 'Commit' && Date.now() >= commitPhaseEndTime) {
-      if (typeof window.ethereum !== 'undefined') {
-        const contract = await initializeProvider();
-        try {
-         
-          // Fetch the highest bid and highest bidder
-          fetchHighestBid();
-          // Update the phase to None
-          setCurrentPhase('-');
-        } catch (e) {
-          console.log('error finalizing auction: ', e);
-        }
+
+  async function fetchBidderCountForDomain(domain) {
+    if (typeof window.ethereum !== 'undefined') {
+      const contract = await initializeProvider();
+      try {
+        // Simulate a delay (e.g., 1 seconds) before fetching the bidder count
+        setTimeout(async () => {
+          console.log(domain);
+          const updatedBidderCount = await contract.getBiddersCountByDomain(domain);
+          console.log('bidderCount:', updatedBidderCount.toNumber());
+          setBidderCount(updatedBidderCount.toNumber());
+        }, 20000); // Adjust the delay time as needed
+       
+      } catch (e) {
+        console.log(`Error fetching bidder count for domain ${domain}:`, e);
       }
     }
   }
-
 //  async function withdraw() {
 //    if (typeof window.ethereum !== 'undefined') {
 //      const contract = await initializeProvider();
@@ -192,8 +260,6 @@ useEffect(() => {
 
   // Fetch initial data
   requestAccount();
-  fetchOwner();
-  fetchMyBid();
   fetchHighestBid();
   fetchOwnedDomains(account);
 
@@ -227,7 +293,25 @@ useEffect(() => {
           {/* Content for the left box */}
           <p>Connected Account: {account}</p>
           <p>My Bid: {myBid}</p>
-    <form onSubmit={(event) => submitBid(event, domain, myBid, 'myhash')}>
+          
+    <form onSubmit={(event) => {
+          event.preventDefault();
+
+          // Validate bid amount
+          const minBidAmount = 1;
+          if (isNaN(myBid) || myBid < minBidAmount) {
+            alert('Minimum bid is 1 wei. Please enter a higher bid.');
+            return;
+          }
+          const convertedBidValue = convertBidToWei();
+          console.log(convertedBidValue);
+
+          // Your existing submitBid logic here
+          submitBid(event, domain, convertedBidValue, 'myhash');
+
+          // Reset form fields
+          
+        }}>
     <div>
     <label htmlFor="domainInput ">Domain Name </label>
       <input
@@ -239,17 +323,26 @@ useEffect(() => {
       placeholder="Enter Domain Name"
       />
       </div>
+      <div className="bid-container">
       <div>
-      <label htmlFor="weiInput">Bid Amount(in wei)  </label>
+      <label htmlFor="EtherInput">Bid Amount </label>
     <input
         id="weiInput"
       value={myBid}
-      onChange={(event) => setMyBid(event.target.value)}
+      onChange={handleBidAmountChange}
       name="Bid Amount"
-      type="number"
+      type="text"
       placeholder="Enter Bid Amount"
       />
        </div>
+       <div>
+          <select id="bidUnit" value={bidAmountUnit} onChange={handleBidUnitChange}>
+            <option value="wei">Wei</option>
+            <option value="gwei">Gwei</option>
+            <option value="ether">Ether</option>
+          </select>
+        </div>
+        </div>
            <button type="submit">Submit</button>
          </form>
         </div>
@@ -262,28 +355,24 @@ useEffect(() => {
           <p>Current Phase: { currentPhase}</p>
           <p>Auction Highest Bid Amount: {highestBid}</p>
        <p>
-         Auction Highest Bidder:{' '}
-         {highestBidder === emptyAddress
-           ? 'null'
-           : highestBidder === account
-           ? 'Me'
-           : highestBidder}
+         Auction Highest Bidder:{highestBidder}
+          
        </p>
         </div>
         
         <div className="big-box right-box">
-        <h2 className="box-headerright">Ongoing bids</h2>
+        <h2 className="box-headerright">Market</h2>
           {/* Content for the right box */}
           <p>Auction Highest Bid Amount: {highestBid}</p>
         </div>
         </div>
       </div>
-      <div className="big-box left-box">
+      <div className="big-box bottom-box">
       <h2 className="box-headerleft">Domains Owned by {account}:</h2>
         {/* Content for the bottom box */}
         <div style={{ marginTop: '20px' }}>
         <p></p>
-        <ul>
+        <ul >
           {ownedDomains.map((domain, index) => (
             <li key={index}>{domain}</li>
           ))}
